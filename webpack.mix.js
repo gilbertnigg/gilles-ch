cfd3a5c738ff23aacb4bee201c1cb9b5f6169cd9
Gilbert Nigg <tech@gilles.ch>
2022-05-27 13:24:32 +0200

    Install Laravel-Mix

---
+++ webpack.mix.js
@@ -0,0 +1,33 @@
+let mix = require('laravel-mix');
+
+mix.options({
+    processCssUrls: false
+  })
+  .setPublicPath(process.env.MIX_DIST)
+  .js(
+    process.env.MIX_SRC + '/scripts/app.js',
+    process.env.MIX_DIST + '/js/'
+  )
+  .postCss(
+    process.env.MIX_SRC + '/styles/app.css',
+    process.env.MIX_DIST + '/css/'
+  )
+  .copyDirectory(
+    process.env.MIX_SRC + '/fonts/',
+    process.env.MIX_DIST + '/fonts/'
+  )
+  .copyDirectory(
+    process.env.MIX_SRC + '/images/',
+    process.env.MIX_DIST + '/img/'
+  )
+  .browserSync({
+    proxy: process.env.MIX_PROXY,
+    files: [
+      process.env.MIX_DIST + '/js/**/*.js',
+      process.env.MIX_DIST + '/css/**/*.css',
+      'public/content/**/*.txt',
+      'public/site/snippets/**/*.php',
+      'public/site/plugins/**/*.php',
+      'public/site/templates/*.php'
+    ],
+  });
