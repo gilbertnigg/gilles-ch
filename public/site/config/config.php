bfedd512c18119dbd28e38761ca1f8ea2702db3f
Gilbert Nigg <30411415+gilbertnigg@users.noreply.github.com>
2022-05-31 13:18:54 +0200

    Setup HTML/Kirby basics

---
+++ public/site/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'debug' => true,
+	'panel' => [
+		'language' => 'de',
+	],
+];
--- public/site/templates/default.php
+++ public/site/templates/default.php
@@ -1,1 +1,18 @@
-<h1><?= $page->title() ?></h1>
+<!DOCTYPE html>
+<html lang="de" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?= $site->title() ?></title>
+	<script type="module">
+		document.documentElement.classList.remove('no-js');
+		document.documentElement.classList.add('js');
+	</script>
+	<?= css('assets/css/app.css') ?>
+	<?= js('assets/js/app.js', ['defer' => true]) ?>
+</head>
+
+<body>
+	<h1><?= $page->title() ?></h1>
+</body>
+</html>
