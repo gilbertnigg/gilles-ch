99cfc0476e14c6c057743de7cd83733c3080001e
Gilbert Nigg <tech@gilles.ch>
2022-06-01 12:30:21 +0200

    Update Service

--- public/site/snippets/service.php
+++ public/site/snippets/service.php
@@ -5,8 +5,8 @@
 	</header>
 	<div class="lg:flex">
 		<div class="lg:w-3/4 lg:pr-16">
-			<p>Bei vielen Projekten, die an mich herangetragen werden, geht es um die Realisierung Websites mit Content Management System (CMS) für ein selbständiges Bearbeiten der Inhalte. Erfolgt die Anfrage über eine Agentur für einen ihrer Kunden, ist ein Designvorschlag oder eine Konzeption in der Regel schon vorhanden. In diesem Fall setze ich in Zusammenarbeit mit den Gestaltern den Entwurf in eine moderne Webseite um. Als CMS verwende ich die Open-Source-Projekte <a href="https://www.processwire.com" class="border-b border-secondary" target="_blank" rel="noopener noreferrer">Processwire</a> oder <a href="https://www.getkirby.com" class="border-b border-secondary" target="_blank" rel="noopener noreferrer">Kirby</a>. Bei Direktkunden entwickle ich bei Bedarf auch das Design, wenn erforderlich im Team mit ausgewählten Grafikerinnen und Grafikern aus meinem langjährigen Netzwerk.</p>
-			<p>Für detaillierte Informationen oder spezifische Bedürfnisse, zögern Sie nicht, mich zu <a href="#kontakt" class="scrollto border-b border-secondary">kontaktieren</a>.</p>
+			<?= $article->text() ?>
+			<p>Für detaillierte Informationen oder spezifische Bedürfnisse, zögern Sie nicht, mich zu <a href="#kontakt">kontaktieren</a>.</p>
 		</div>
 		<aside class="mt-8 lg:mt-0 lg:pl-2 lg:w-1/4">
 			<h4 class="mt-8 md:mt-0">Leistungen</h4>
