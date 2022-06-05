625d914bd583dc2e4a32df1a6baa1bc485951294
Gilbert Nigg <30411415+gilbertnigg@users.noreply.github.com>
2022-06-05 13:05:08 +0200

    Fix FF width

--- public/site/snippets/navicon.php
+++ public/site/snippets/navicon.php
@@ -1,4 +1,4 @@
-<button id="navicon" class="fixed z-50 inset-x-0 top-0 py-[2px] bg-black xl:hidden" aria-label="Menu öffnen/schliessen">
+<button id="navicon" class="fixed z-50 w-full left-0 top-0 py-[2px] bg-black xl:hidden" aria-label="Menu öffnen/schliessen">
     <span class="container block mx-auto px-4">
 		<span class="block w-6">
 			<span class="block my-[3px] h-px bg-gb"></span>
