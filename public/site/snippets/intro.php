7671ce2e8cee6525e7e79bfd2f5062ac80c1aa37
Gilbert Nigg <30411415+gilbertnigg@users.noreply.github.com>
2022-06-14 19:56:53 +0200

    Add some &shy; for better word-break

--- public/site/snippets/intro.php
+++ public/site/snippets/intro.php
@@ -5,8 +5,8 @@
 	</header>
 	<h5 class="font-light text-gb-dark text-xl md:font-thin md:text-2xl lg:text-3xl">
 		Willkommen bei Ihrem Partner für die Um&shy;setzung unter&shy;schied&shy;lichster Web&shy;lösungen.
-		Für Agenturen als auch für Direkt&shy;kunden – von der Konzeption bis zur erfolg&shy;reichen
-		Online-Schaltung und Betreuung. <span class="text-gb">Schön, sind Sie hier.</span>
+		Für Agenturen als auch für Direkt&shy;kunden – von der Kon&shy;zeption bis zur erfolg&shy;reichen
+		Online-Schaltung und Be&shy;treu&shy;ung. <span class="text-gb">Schön, sind Sie hier.</span>
 	</h5>
 	<ul class="tags mt-8">
 		<li>
