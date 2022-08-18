79e04da523a1dcd8fa2ab1c0f9fcc409df5eecbd
Gilbert Nigg <30411415+gilbertnigg@users.noreply.github.com>
2022-08-19 00:32:32 +0200

    Update kontakt DOM

--- public/site/templates/kontakt.php
+++ public/site/templates/kontakt.php
@@ -1,11 +1,24 @@
-<?php snippet('head') ?>
+<!DOCTYPE html>
+<html lang="de" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex, nofollow">
+	<title><?= $site->seo_title() ?></title>
+	<meta name="description" content="<?= $site->seo_description() ?>">
+	<script type="module">
+		document.documentElement.classList.remove('no-js');
+		document.documentElement.classList.add('js');
+	</script>
+	<?= css('assets/css/app.css') ?>
+	<script src="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js"></script>
+	<link href="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css" rel="stylesheet" />
+</head>
 
-<body class="bg-gray-200 text-gb-dark text-base antialiased">
-<script src="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js"></script>
-<link href="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css" rel="stylesheet" />
+<body class="bg-gray-200 text-base antialiased">
 
 <main class="block">
-<div id="map" class="w-screen h-screen bg-gb-dark text-white"></div>
+<div id="map" class="w-screen h-screen"></div>
 <script>
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ25pZ2ciLCJhIjoiY2wydTM3NGlwMGFkNDNzbzQ2N2doZ3loYyJ9._yiYy6qVqHCBvGtH5ciOTA';
 const map = new mapboxgl.Map({
