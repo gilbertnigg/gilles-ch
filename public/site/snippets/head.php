f911fa6d3898444d5595530bc09b7142413d8d1b
Gilbert Nigg <tech@gilles.ch>
2023-05-04 10:41:16 +0200

    Matomo local

--- public/site/snippets/head.php
+++ public/site/snippets/head.php
@@ -39,11 +39,11 @@
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="https://gilles.matomo.cloud/";
+    var u="//www.gilles.ch/analytics/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='//cdn.matomo.cloud/gilles.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
 <!-- End Matomo Code -->
