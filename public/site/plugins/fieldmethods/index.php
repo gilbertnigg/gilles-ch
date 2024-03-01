73017bb0607c7687fb8ceabe41df70ff6476ce91
Gilbert Nigg <tech@gilles.ch>
2024-03-01 12:47:24 +0100

    Add field methods

---
+++ public/site/plugins/fieldmethods/index.php
@@ -0,0 +1,14 @@
+<?php
+
+Kirby::plugin('gilles/fieldextensions', [
+    'fieldMethods' => [
+        'toTitles' => function ($field) {
+            $out = [];
+			foreach ($field->split(',') as $item) {
+				$out[] = page($item)->title();
+			}
+			$out = implode(', ', $out);
+            return $out;
+        }
+    ]
+]);
