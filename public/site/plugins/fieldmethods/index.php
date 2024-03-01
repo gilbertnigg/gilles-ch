e1d1c7001f703610b1d417f4e3065a5c97be4370
Gilbert Nigg <tech@gilles.ch>
2024-03-01 13:01:20 +0100

    Better url

--- public/site/plugins/fieldmethods/index.php
+++ public/site/plugins/fieldmethods/index.php
@@ -4,14 +4,15 @@ Kirby::plugin('gilles/fieldextensions', [
     'fieldMethods' => [
         'toTitles' => function ($field) {
             $out = [];
-			foreach ($field->split(',') as $item) {
-				$out[] = page($item)->title();
-			}
-			$out = implode(', ', $out);
+            foreach ($field->split(',') as $item) {
+            	$out[] = page($item)->title();
+            }
+            $out = implode(', ', $out);
             return $out;
         },
         'toUrl' => function ($field) {
-            $out = str_replace(['http://', 'https://', 'www.'], '', $field);
+            $link = str_replace(['http://', 'https://', 'www.'], '', $field);
+            $out = '<a href="'.$field->value.'" target="_blank" rel="noopener noreferrer">'.$link.'</a>';
             return $out;
         }
     ]
